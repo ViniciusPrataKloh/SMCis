e358f684f5e26f1f4cf71e8b62a88b8c0265fad6
ViniciusPrataKloh <vinicius.prata.kloh@gmail.com>
2018-02-26 13:53:35 -0300

    SMCis

--- cpu/temp.sh
+++ cpu/temp.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 IPMI_TEMP_OUTPUT=`/usr/sbin/ipmi-sensors`
-### Power and Temperature ###
 CURRENT_TEMP1=$(echo $IPMI_TEMP_OUTPUT | /usr/sbin/ipmi-sensors | grep CPU1 | grep Temp | awk '{print $8}')
 CURRENT_TEMP2=$(echo $IPMI_TEMP_OUTPUT | /usr/sbin/ipmi-sensors | grep CPU2 | grep Temp | awk '{print $8}')
 
