44f35a60bf9274ce7656d9a317ac22907e98874b
ViniciusPrataKloh <vinicius.prata.kloh@gmail.com>
2018-03-02 13:21:44 -0300

    SMCis

--- gpu/parse_json_GPU.cpp
+++ gpu/parse_json_GPU.cpp
@@ -12,7 +12,7 @@ int main(int argc, char **argv)
    char aux[10][25], timeStamp[3][30], dev_name[4][10], Tparse[4][4];
    int sensorData[5], dev_n[2], usage[4], stime[4], ctime[4], timems = 0;
    float memory[6];
-   
+
    if(argc > 1)
    {
       strcpy(inputFile, argv[1]);
@@ -21,8 +21,6 @@ int main(int argc, char **argv)
    {
       strcpy(inputFile, "sensor.out");
    }
-   
-//   printf("%s\n", inputFile);
 
    inPtr = fopen(inputFile, "rt");
    if(inPtr==NULL)
@@ -30,21 +28,20 @@ int main(int argc, char **argv)
       printf("Falha na abertura");
       return 1;
    }
-   
-   outPtr = fopen("dataTable.json", "wt");
+
+   outPtr = fopen("parsed.json", "wt");
    if(outPtr==NULL)
    {
       printf("Falha na abertura");
       return 1;
    }
-   
+
    fscanf(inPtr, "%s %s", aux[0], timeStamp[0]);
    fscanf(inPtr, "%s %d", aux[9], &sensorData[4]);
    fscanf(inPtr, "%s %s %d", aux[1], aux[2], &sensorData[0]);
    fscanf(inPtr, "%s %s %d", aux[3], aux[4], &sensorData[1]);
    fscanf(inPtr, "%s %s %d", aux[5], aux[6], &sensorData[2]);
    fscanf(inPtr, "%s %s %d", aux[7], aux[8], &sensorData[3]);
-//   fscanf(inPtr, "%s %d", aux[9], &sensorData[4]);
 
    fscanf(inPtr, "%s", timeStamp[1]);
 
@@ -72,12 +69,7 @@ int main(int argc, char **argv)
    aux[8][0] = tolower(aux[8][0]);
 
    timems = (3600000*(ctime[0]-stime[0]))+(60000*(ctime[1]-stime[1]))+(1000*(ctime[2]-stime[2]))+(ctime[3]-stime[3]);  
-/*
-printf("%s\n%s\n%s\n%s\n%s\n",Tparse[0],Tparse[1],Tparse[2],Tparse[3],timeStamp[0]);
-printf("%d:%d:%d.%d\n",stime[0],stime[1],stime[2],stime[3]);
-printf("%d:%d:%d.%d\n",ctime[0],ctime[1],ctime[2],ctime[3]);
-printf("%d\n", timems);
-*/
+
    fscanf(inPtr, "%s %s", dev_name[0], dev_name[1]);
    fscanf(inPtr, "%d", &dev_n[0]);
    fscanf(inPtr, "%f %f %f", &memory[0], &memory[1], &memory[2]);
@@ -90,21 +82,10 @@ printf("%d\n", timems);
    fscanf(inPtr, "%d %d", &usage[2], &usage[3]);
 
 
-//   printf("| %s |     %03d     |     %03d     |     %03d     |     %03d     |      %04d      |", timeStamp[0], sensorData[0], sensorData[1], sensorData[2], sensorData[3], sensorData[4]);
-
-//   printf(" %s | %s %s | %d | %.2f | %.2f | %.2f | %d%% | %d%% |", timeStamp[1], dev_name[0], dev_name[1], dev_n[0], memory[0], memory[1], memory[2], usage[0], usage[1]);
-
-//   printf(" %s | %s %s | %d | %.2f | %.2f | %.2f | %d%% | %d%% | \n", timeStamp[2], dev_name[2], dev_name[3], dev_n[1], memory[3], memory[4], memory[5], usage[2], usage[3]);
-
-
-//    fprintf(outPtr,"| %s | %s %s  | %s %s  | %s %s  | %s %s  | %s  |\n");
-//    fprintf(outPtr,"| %d | %d     | %d     | %d     | %d     | %d  |\n");
-//    fprintf(outPtr,"|        %s       |  %s %s  |  %s %s  |  %s %s  | Core0 usage (%%) | memory0 usage (%%) |  %s %s  | Core1 usage (%%) | memory1 usage (%%) |  %s  |\n",
-//	  aux[0],"CPU0",aux[2],"CPU1",aux[4],"GPU0",aux[6],"GPU1",aux[8],aux[9]);
-   fprintf(outPtr,"[\n");  
+   fprintf(outPtr,"[\n");
 
  fprintf(outPtr,"{\"%s\": %d, \"%s_%s\": %d, \"%s_%s\": %d, \"%s_%s\": %d, \"core0_usage\": %d, \"memory0_usage\": %d, \"%s_%s\": %d, \"core1_usage\": %d, \"memory1_usage\": %d, \"%s\": %d}, \n",aux[0], timems, "cpu0",aux[2], sensorData[0], "cpu1",aux[4], sensorData[1], "gpu0",aux[6], sensorData[3], usage[1], usage[0], "gpu1",aux[8], sensorData[2],usage[3], usage[2], aux[9], sensorData[4]);
-   
+
    while(!feof(inPtr))
    {
       fscanf(inPtr, "%s %s", aux[0], timeStamp[0]);
@@ -152,29 +133,12 @@ printf("%d\n", timems);
 
       timems = (3600000*(ctime[0]-stime[0]))+(60000*(ctime[1]-stime[1]))+(1000*(ctime[2]-stime[2]))+(ctime[3]-stime[3]);
 
-      fprintf(outPtr,"{\"%s\": %d, \"%s_%s\": %d, \"%s_%s\": %d, \"%s_%s\": %d, \"core0_usage\": %d, \"memory0_usage\": %d, \"%s_%s\": %d, \"core1_usage\": %d, \"memory1_usage\": %d, \"%s\": %d}, \n",aux[0], timems, "cpu0",aux[2], sensorData[0], "cpu1",aux[4], sensorData[1], "gpu0",aux[6], sensorData[3], usage[1], usage[0], "gpu1",aux[8], sensorData[2],usage[3], usage[2], aux[9], sensorData[4]);
-      
-/*
-printf("%s\n%s\n%s\n%s\n%s\n",Tparse[0],Tparse[1],Tparse[2],Tparse[3],timeStamp[0]);
-printf("%d:%d:%d.%d\n",stime[0],stime[1],stime[2],stime[3]);
-printf("%d:%d:%d.%d\n",ctime[0],ctime[1],ctime[2],ctime[3]);
-printf("%d\n", timems);
-
-sleep(100);
-
-      fprintf(outPtr,"|  %s  | %11d | %11d | %11d | %15d | %17d | %11d | %15d | %17d | %14d |\n", timeStamp[1], sensorData[0], sensorData[1], sensorData[3], usage[1], usage[0], sensorData[2], usage[3], usage[2], sensorData[4]);
-*/
-
-/*      fprintf(outPtr,"| %s |     %03d     |     %03d     |     %03d     | %d%% | %d%% |     %03d     | %d%% | %d%% |      %04d      |\n",
-                     timeStamp[1], sensorData[0], sensorData[1], sensorData[3], usage[0], usage[1], sensorData[2],usage[2], usage[3], sensorData[4]);
-    
-      fprintf(outPtr,"| %s |     %03d     |     %03d     |     %03d     |     %03d     |      %04d      |\n", 
-	  timeStamp[0], sensorData[0], sensorData[1], sensorData[2], sensorData[3], sensorData[4]);
-*/
+      fprintf(outPtr,"{\"%s\": %d, \"%s_%s\": %d, \"%s_%s\": %d, \"%s_%s\": %d, \"core0_usage\": %d, \"memory0_usage\": %d, \"%s_%s\": %d, \"core1_usage\": %d, \"memory1_usage\": %d, \"%s\": %d}", aux[0], timems, "cpu0",aux[2], sensorData[0], "cpu1",aux[4], sensorData[1], "gpu0",aux[6], sensorData[3], usage[1], usage[0], "gpu1",aux[8], sensorData[2],usage[3], usage[2], aux[9], sensorData[4]);
+      fprintf(outPtr, feof(inPtr) ? "] \n" : ", \n");
    }
-   
+
    fclose(inPtr);
    fclose(outPtr);
-   
+
    return 0;
 }
